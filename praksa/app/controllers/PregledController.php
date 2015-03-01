33437dc066673582df24546f03bced2544c2762c
Lvuletic <luka.vuleti@gmail.com>
2015-03-01 21:47:10 +0100

    Konacna verzija kontrolera za prikaz narudzbi

--- praksa/app/controllers/PregledController.php
+++ praksa/app/controllers/PregledController.php
@@ -10,51 +10,39 @@ class PregledController extends \Phalcon\Mvc\Controller
 {
     public function indexAction()
     {
-        /*if ($this->request->isPost()==true)
-        {
-            echo "da";
-            if ($this->request->isAjax()==true)
-            {
-                echo "takodjer da";
-            }
-        }
-        else echo "ne";
-        $sifra = 5;
-        $sifra = $this->request->getPost("sifra");
-        echo $sifra;
-        echo "gfdg";
-        echo $this->request->getPost("sifra");*/
+
     }
 
     public function izracunAction()
     {
         ?>
-        <div id="scroll2">
-            <table id="stavke" border="1">
+        <div id="odabir">
+        <table id="stavke" border="1">
 
-                <tr>
-                    <th>Sifra stavke</th>
-                    <th>Sifra artikla</th>
-                    <th>Naziv artikla</th>
-                    <th>Ukupna cijena</th>
-                </tr>
-                <?php
-                $sifra = $this->request->getPost("stavkaN");
-                $stavke = Stavka::find("sifra_N='$sifra'");
-                foreach($stavke as $stavka)
-                {
-                    ?>
-                    <tr>
-                        <td><?php echo $stavka->sifra_S?></td>
-                        <td><?php echo $stavka->sifra_N?></td>
-                        <td><?php echo $stavka->naziv_A?></td>
-                        <td><?php echo $stavka->ukupna_cijena?></td>
-                    </tr>
-                <?php
+        <tr>
+            <th>Sifra stavke</th>
+            <th>Sifra artikla</th>
+            <th>Naziv artikla</th>
+            <th>Cijena</th>
+        </tr>
+        <?php
+        $request = new Phalcon\Http\Request();
+        $sifra = $request->get('sifra');
+        $stavke = Stavka::find("sifra_N='$sifra'");
+        foreach($stavke as $stavka)
+        {
+            ?>
+            <tr>
+                <td><?php echo $stavka->sifra_S?></td>
+                <td><?php echo $stavka->sifra_A?></td>
+                <td><?php echo $stavka->naziv_A?></td>
+                <td><?php echo $stavka->ukupna_cijena?></td>
+            </tr>
+        <?php
 
-                }
-                ?>
-            </table>
+        }
+        ?>
+        </table>
 
         </div>
     <?php
