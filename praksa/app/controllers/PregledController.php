884666606003ff995a10f5045aab6acfb77539db
Lvuletic <luka.vuleti@gmail.com>
2015-03-01 21:30:03 +0100

    Donekle funkcionalna verzija koja uspješno prikazuje narudžbe

--- praksa/app/controllers/PregledController.php
+++ praksa/app/controllers/PregledController.php
@@ -10,19 +10,55 @@ class PregledController extends \Phalcon\Mvc\Controller
 {
     public function indexAction()
     {
-
+        /*if ($this->request->isPost()==true)
+        {
+            echo "da";
+            if ($this->request->isAjax()==true)
+            {
+                echo "takodjer da";
+            }
+        }
+        else echo "ne";
+        $sifra = 5;
+        $sifra = $this->request->getPost("sifra");
+        echo $sifra;
+        echo "gfdg";
+        echo $this->request->getPost("sifra");*/
     }
 
     public function izracunAction()
     {
+        ?>
+        <div id="scroll2">
+            <table id="stavke" border="1">
 
-        $sifra = $this->request->getPost("sifra");
-        $stavke = Stavka::find("sifra_N='$sifra'");
-        foreach($stavke as $stavka)
-        {
-            echo $stavka->naziv_A;
-        }
-        dodajRed("aaa");
+                <tr>
+                    <th>Sifra stavke</th>
+                    <th>Sifra artikla</th>
+                    <th>Naziv artikla</th>
+                    <th>Ukupna cijena</th>
+                </tr>
+                <?php
+                $sifra = $this->request->getPost("stavkaN");
+                $stavke = Stavka::find("sifra_N='$sifra'");
+                foreach($stavke as $stavka)
+                {
+                    ?>
+                    <tr>
+                        <td><?php echo $stavka->sifra_S?></td>
+                        <td><?php echo $stavka->sifra_N?></td>
+                        <td><?php echo $stavka->naziv_A?></td>
+                        <td><?php echo $stavka->ukupna_cijena?></td>
+                    </tr>
+                <?php
+
+                }
+                ?>
+            </table>
+
+        </div>
+    <?php
+
 
     }
 }
